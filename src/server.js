05ba6934a94795e79d29d3fb9b7d4422e62722a7
Vu Ho <vh.ho8160@gmail.com>
2022-05-10 22:09:56 -0400

    mirror update

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ let app = express();
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', process.env.URL_REACT);
+  res.setHeader('Access-Control-Allow-Origin', process.env.REACT_APP_FRONTEND_URL);
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
