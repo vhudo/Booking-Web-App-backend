5919db5d8d9d882b39f58c3b341fdabaedc4d326
Vu Ho <vh.ho8160@gmail.com>
2022-05-22 13:26:30 -0400

    Create migration-create-markdown.js

---
+++ src/migrations/migration-create-markdown.js
@@ -0,0 +1,45 @@
+"use strict";
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable("Clinics", {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            contentHTML: {
+                allowNull: false,
+                type: Sequelize.TEXT('long'),
+            },
+            contentMarkdown: {
+                allowNull: false,
+                type: Sequelize.TEXT('long'),
+            },
+            description: {
+                type: Sequelize.TEXT('long'),
+            },
+            doctorId: {
+                type: Sequelize.INTEGER,
+            },
+            clinicId: {
+                type: Sequelize.INTEGER,
+            },
+            specialtyId: {
+                type: Sequelize.INTEGER,
+            },
+
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        });
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable("Clinics");
+    },
+};
