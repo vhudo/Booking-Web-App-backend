1056cebeb02a7af8acedcad29386ca803a684c84
Vu Ho <vh.ho8160@gmail.com>
2022-05-22 20:53:40 -0400

    Update web.js
    create getDetailDoctor

--- src/route/web.js
+++ src/route/web.js
@@ -7,22 +7,24 @@ let router = express.Router();
 let initWebRoutes = (app) => {
   router.get("/", homeController.getHomePage);
   router.get("/crud", homeController.getCRUD);
-
   router.get("/get-crud", homeController.displayCRUD);
   router.get("/edit-crud", homeController.getEditCRUD);
   router.post("/post-crud", homeController.postCRUD);
-
   router.post("/put-crud", homeController.putCRUD);
   router.get("/delete-crud", homeController.deleteCRUD);
 
-  router.post('/api/login', userController.handleLogin);
-  router.get('/api/get-user', userController.handleGetUsers);
-  router.post('/api/create-new-user', userController.handleCreateNewUser);
-  router.put('/api/update-user', userController.handleUpdateUser);
-  router.delete('/api/delete-user', userController.handleDeleteUser);
+  router.post("/api/login", userController.handleLogin);
+  router.get("/api/get-user", userController.handleGetUsers);
+  router.post("/api/create-new-user", userController.handleCreateNewUser);
+  router.put("/api/update-user", userController.handleUpdateUser);
+  router.delete("/api/delete-user", userController.handleDeleteUser);
+  router.get("/api/allcode", userController.getAllCode);
 
-  router.get('/api/allcode', userController.getAllCode);
-  router.get('/api/home-top-doctor', doctorController.getTopDoctorHome);
+  router.get("/api/home-top-doctor", doctorController.getTopDoctorHome);
+  router.get("/api/get-all-doctors", doctorController.getAllDoctors);
+  router.post("/api/save-info-doctor", doctorController.postSaveInfoDoctor);
+  router.get("/api/get-detail-doctor", doctorController.getDetailDoctor);
+
 
   return app.use("/", router);
 };
