c84744b692ec55b846b86d7fb36bea33e4bafe72
Vu Ho <vh.ho8160@gmail.com>
2022-05-22 20:55:22 -0400

    Update doctorController.js

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -1,25 +1,62 @@
-import doctorService from '../services/doctorService'
+import doctorService from "../services/doctorService";
 
 let getTopDoctorHome = async (req, res) => {
-    let limit = req.query.limit
-    if (!limit) limit = 10
-    try {
-        let response = await doctorService.getTopDoctorHome(+limit)
-        return res.status(200).json(response)
-
-    } catch (e) {
-        console.log(e)
-        return res.status(200).json({
-            errCode: -1,
-            message: 'error from server',
-        })
-    }
-
+  let limit = req.query.limit;
+  if (!limit) limit = 10;
+  try {
+    let response = await doctorService.getTopDoctorHome(+limit);
+    return res.status(200).json(response);
+  } catch (e) {
+    console.log(e);
+    return res.status(200).json({
+      errCode: -1,
+      message: "error from server",
+    });
+  }
 };
 
+let getAllDoctors = async (req, res) => {
+  try {
+    let doctors = await doctorService.getAllDoctors();
+    return res.status(200).json(doctors);
+  } catch (e) {
+    console.log(e);
+    return res.status(200).json({
+      errCode: -1,
+      message: "error from server",
+    });
+  }
+};
 
+let postSaveInfoDoctor = async (req, res) => {
+  try {
+    let response = await doctorService.saveInfoDoctor(req.body);
+    return res.status(200).json(response);
+  } catch (e) {
+    console.log(e);
+    return res.status(200).json({
+      errCode: -1,
+      message: "error from server",
+    });
+  }
+};
+
+let getDetailDoctor = async (req, res) => {
+  try {
+    let information = await doctorService.getDetailDoctor(req.query.id);
+    return res.status(200).json(information);
+  } catch (e) {
+    console.log(e);
+    return res.status(200).json({
+      errCode: -1,
+      message: "error from server",
+    });
+  }
+};
 
 module.exports = {
-    getTopDoctorHome: getTopDoctorHome,
-
-}
+  getTopDoctorHome: getTopDoctorHome,
+  getAllDoctors: getAllDoctors,
+  postSaveInfoDoctor: postSaveInfoDoctor,
+  getDetailDoctor: getDetailDoctor,
+};
